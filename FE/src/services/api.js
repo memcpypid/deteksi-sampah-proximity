4d06bd65ea79744142b8fe5c68499a9e5196f649
DarmaPutra <darmaputra443@gmail.com>
2025-10-08 14:54:18 +0700

    Change Domain

--- FE/src/services/api.js
+++ FE/src/services/api.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
-
+import axios from "axios";
+const baseURL = "https://deteksisampah.site/api";
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: baseURL,
   timeout: 15000,
 });
 
 export async function getSamples(params = {}) {
-  const res = await api.get('/samples', { params });
+  const res = await api.get("/samples", { params });
   return res.data || [];
 }
 
 export async function getHistory() {
-  const res = await api.get('/history');
+  const res = await api.get("/history");
   return res.data || [];
 }
 
 export async function clearHistory() {
-  const res = await api.delete('/clear_history');
+  const res = await api.delete("/clear_history");
   return res.data;
 }
 
--- FE/src/services/websocket.js
+++ FE/src/services/websocket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const DEFAULT_SOCKET_URL = `${window.location.protocol}//${window.location.hostname}:5000`;
+const DEFAULT_SOCKET_URL = "https://deteksisampah.site";
 const SOCKET_URL =
   (import.meta && import.meta.env && import.meta.env.VITE_SOCKET_URL) ||
   DEFAULT_SOCKET_URL;
